935801c83f7a94cf9b1b2884156029010fc5a80b
Justin Pujols <Justin Pujols@DESKTOP-LPRCCFD>
2025-04-21 09:38:46 -0400

    oferta, falta terminar

---
+++ src/Datos/vOferta.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Datos;
+
+/**
+ *
+ * @author Justin Pujols
+ */
+public class vOferta {
+    
+    private int id;
+    private String nombre_oferta;
+    private String descripcion;
+    private double descuento_porcentaje;
+    
+    
+}
